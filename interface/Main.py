f89b8db171985355631b4ff2bc0d63d8ee904150
Erika Azevedo <erikaa.chang@gmail.com>
2025-03-21 14:08:25 -0300

    with post for api

--- interface/Main.py
+++ interface/Main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import requests
 
 st.title('Eyesense 👁️')
 
@@ -10,8 +11,26 @@ st.text("Simply upload your image and click 'Predict!' 🧙‍♀️ ")
 image_file = st.file_uploader("Upload you image file here:", accept_multiple_files=False, type=['jpeg', 'png', 'jpg'])
 
 if image_file:
-    st.image(image_file)
+    col1, col2 = st.columns(2)
 
-st.button("Predict! 🧙‍♀️")
+    with col1:
+    ### Display the image user uploaded
+        st.image(image_file, caption="Here's the image you uploaded ☝️")
+
+    with col2:
+        with st.spinner("Wait for it..."):
+        ### Get bytes from the file buffer
+            img_bytes = image_file.getvalue()
+
+
+if st.button("Predict! 🧙‍♀️"):
+    url = 'https://taxifare.lewagon.ai/predict'
+    response = requests.post(url + "/predict", files={'img': img_bytes})
+    if response.status_code == 200:
+        prediction = response.json().get("result", "Error: No fare returned")
+        st.success(f"Estimated Fare: ${prediction:.2f}")
+    else:
+        st.error("Error fetching prediction. Please try again.")
+
 
 st.button("Numbers for nerds 🤓")
